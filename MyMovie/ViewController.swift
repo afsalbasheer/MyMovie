4cdf7d38cd0f7b5414c2f66fb951e587f57203f9
Afsal <afzalleenabasheer@gmail.com>
2024-01-10 15:10:53 +0400

    Initial Commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by Afsal on 10/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
